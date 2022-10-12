c331730e0b8eae81ff32e826d678aa9c1f377ffd
joaonevesf <joaodnfernandes99@gmail.com>
2022-10-12 15:28:19 +0100

    Added numbers

---
+++ src/test/java/com/aor/numbers/ListDeduplicatorTest.java
@@ -0,0 +1,49 @@
+package com.aor.numbers;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ListDeduplicatorTest {
+
+    List<Integer> list;
+
+    @Test
+    public void deduplicate() {
+        class StubListSorter implements GenericListSorter {
+            @Override
+            public List<Integer> sort(List<Integer> list){
+                return Arrays.asList(1,2,4,5);
+            }
+        }
+        StubListSorter sorter = new StubListSorter();
+        List<Integer> expected = Arrays.asList(1,2,4,5);
+        ListDeduplicator deduplicator = new ListDeduplicator(sorter);
+        List<Integer> distinct = deduplicator.deduplicate(list);
+
+        Assertions.assertEquals(expected, distinct);
+    }
+
+    @Test
+    public void bug_deduplicate_8726() {
+        List<Integer> list = Arrays.asList(1, 2, 4, 2);
+        class StubListSorter implements GenericListSorter {
+            @Override
+            public List<Integer> sort(List<Integer> list) {
+                return Arrays.asList(1, 2, 2, 4);
+            }
+        }
+        StubListSorter sorter = new StubListSorter();
+        ListDeduplicator deduplicator = new ListDeduplicator(sorter);
+        List<Integer> distinct = deduplicator.deduplicate(list);
+        Assertions.assertEquals(Arrays.asList(1, 2, 4), distinct);
+    }
+
+    @BeforeEach
+    public void helper() {
+        list = Arrays.asList(1,2,4,2,5);
+    }
+}
---
+++ src/test/java/com/aor/numbers/ListSorterTest.java
@@ -0,0 +1,36 @@
+package com.aor.numbers;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+public class ListSorterTest {
+
+    List<Integer> list;
+
+    @Test
+    public void sort() {
+        List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+
+        ListSorter sorter = new ListSorter();
+        List<Integer> sorted = sorter.sort(list);
+
+        Assertions.assertEquals(expected, sorted);
+    }
+
+    @Test
+    public void bug_sorter_8726() {
+        List<Integer> list = Arrays.asList(1,2,4,2);
+        ListSorter sorter = new ListSorter();
+        List<Integer> sorted = sorter.sort(list);
+
+        Assertions.assertEquals(Arrays.asList(1,2,2,4),sorted);
+    }
+
+    @BeforeEach
+    public void helper() {
+        list = Arrays.asList(3, 2, 6, 1, 4, 5, 7);
+    }
+}
